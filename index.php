3d11fca1a8dc3a7ed1330d0076fd744c7565349e
Kronwarin <42400942+Kronwarin@users.noreply.github.com>
2021-01-21 09:40:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<fieldset class="m_still"><legend><b>kronwarin putthawong 6008111001</b></legend>
+<table style="width:0px;float:left;"class="m_hidden"><tr><td valign="top">
+<a href="15078585_1777973142469616_6448315065094069283_n.jpg">
+<img src="https://kronwarin.github.io/mytest/index.files/image001.jpg" border="0" width="80" /></a>
+</td></tr></table>
+<table class="m_min300"><tr><td valign="top">
+<p style="font-family:FC Knomphing Regular;font-size:29px;font-weight:bold;">
+<br>สวัสดีค่ะทุกท่านที่เข้ามา  
+</br>
+ดิฉันชื่อนางสาวกรวรินทร์ พุทธวงค์ (น้ำหวาน)  
+</br>
+กำลังศึกษาอยู่มหาวิทยาลัยเนชั่น จ.ลำปาง ชั้นปีที่2   </p>
+</td></tr></table>
+<table class="m_min300"><tr><td valign="top">
+<img src="welcome.jpg" width="300" border="0" /></a>
+<br/><a href=""></a>
+</td></tr></table>
